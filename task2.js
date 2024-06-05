135734e3b68fb61819c91c19812c505ad13a323a
muhammad ashari <122855691+ashari-dev@users.noreply.github.com>
2024-06-05 17:14:27 +0700

    task2.js

--- task2.js
+++ task2.js
@@ -1,16 +1,89 @@
-const names = [
-  "Abigail","Alexandra","Alison","Amanda",
-  "Angela","Bella","Carol","Caroline",
-  "Carolyn","Deirdre","Diana","Elizabeth",
-  "Ella","Faith","Olivia","Penelope",
-];
 
-function searchNames(search, jumMax) {
-  const filterNama = names.filter(function (name) {
-    name.toLowerCase().includes(search.toLowerCase());
-  });
-  return filterNama.slice(0, jumMax);
+const names = [
+  "Abigail",
+  "alexandra",
+  "Alison",
+  "Amanda",
+  "Angela",
+  "Bella",
+  "Carol",
+  "Caroline",
+  "Carolyn",
+  "Deirdre",
+  "Diana",
+  "Elizabeth",
+  "Ella",
+  "Faith",
+  "Olivia",
+  "Penelope",
+];
+const hurufKecil = {
+  A: "a",
+  B: "b",
+  C: "c",
+  D: "d",
+  E: "e",
+  F: "f",
+  G: "g",
+  H: "h",
+  I: "i",
+  J: "j",
+  K: "k",
+  L: "l",
+  M: "m",
+  N: "n",
+  O: "o",
+  P: "p",
+  Q: "q",
+  R: "r",
+  S: "s",
+  T: "t",
+  U: "u",
+  V: "v",
+  W: "w",
+  X: "x",
+  Y: "y",
+  Z: "z",
+};
+
+function searchName(search, arr, callback) {
+  //membuat search menjadi huruf kecil semua
+  let cari = "";
+  for (let i = 0; i < search.length; i++) {
+    const karakter = search[i];
+    cari += hurufKecil[karakter] || karakter;
+  }
+
+  // mengubah isi dalam array menjadi huruf kecil semua
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    let pecahArr = arr[i];
+
+    let kata = "";
+    for (let i = 0; i < pecahArr.length; i++) {
+      const karakter = pecahArr[i];
+      kata += hurufKecil[karakter] || karakter;
+    }
+    result = [...result, kata];
+  }
+
+  // melakukan filterasi
+  let container = [];
+  for (let i = 0; i < result.length; i++) {
+    if (result[i].includes(cari)) {
+      container = [...container, result[i]];
+    }
+  }
+  callback(container, 3);
 }
 
-const hasilPencarian = searchNames("an", 3);
-console.log(hasilPencarian);
+function limit(arr, lim) {
+  let penampung = [];
+  for (let i = 0; i < lim; i++) {
+    if (arr[i] !== undefined) {
+      penampung = [...penampung, arr[i]];
+    }
+  }
+  console.log(penampung);
+}
+searchName("La", names, limit);
